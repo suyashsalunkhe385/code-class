645a814947f0ba5e77f5155be7f5f5b48370864b
suyash salunkhe <suyashsalunkhe385@gmail.com>
2026-09-23 22:20:09 +0530

    programno16:day16

---
+++ programno16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() { cout << "Base constructor" << endl; }
+    ~Base() { cout << "Base destructor" << endl; }
+};
+
+class Derived : public Base {
+public:
+    Derived() { cout << "Derived constructor" << endl; }
+    ~Derived() { cout << "Derived destructor" << endl; }
+};
+
+int main() {
+    Derived d;
+    return 0;
+}
