283ceece690b7f087e7d781d1ccb3b3df4fc8152
suyash salunkhe <suyashsalunkhe385@gmail.com>
2026-09-24 11:43:05 +0530

    programno18:day18

---
+++ programno18.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+public:
+    virtual void move() { cout << "Vehicle is moving" << endl; }
+    virtual ~Vehicle() {}
+};
+
+class Car : public Vehicle {
+public:
+    void move() override { cout << "Car moves on roads" << endl; }
+};
+
+class Boat : public Vehicle {
+public:
+    void move() override { cout << "Boat moves on water" << endl; }
+};
+
+int main() {
+    Car c;
+    Boat b;
+    c.move();
+    b.move();
+    return 0;
+}
